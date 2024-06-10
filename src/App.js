9930ed324aaba11a2083d34c4ad73b28d631e1a7
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-06-10 16:55:21 +0530

    Add basic forms

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import GeneralInfo from './components/GeneralInfo';
+import Education from './components/Education';
+import Experience from './components/Experience';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="form">
+        <GeneralInfo />
+        <Education />
+        <Experience />
+      </div>
+
+      <div className="resume">
+      </div>
     </div>
   );
 }
