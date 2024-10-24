d47819fa9c2d63b1c5e4a1e98b106c7821db1f34
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-10-25 00:46:34 +0530

    Created footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+const Footer = () => {
+	  return (
+	<div class="footer">
+        <div class="footer-content">
+            <p>Created by Prajwal Tiwari</p>
+        </div>
+        <a class="git" href="https://github.com/Jwoll2004/Library" target="_blank">
+            <p>GitHub</p>
+        </a>
+    </div>
+  );
+};
+
+export default Footer;
