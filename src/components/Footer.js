88f19dbcb422dbe05ea98496a2b3f93150b11ea2
Prajwal Tiwari <fnafchica2004@gmail.com>
2024-11-04 19:44:31 +0530

    Updated footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,15 @@
+import github from "../assets/images/githubmono.svg";
+
+
 const Footer = () => {
 	  return (
 	<div class="footer">
         <div class="footer-content">
             <p>Created by Prajwal Tiwari</p>
         </div>
-        <a class="git" href="https://github.com/Jwoll2004/Library" target="_blank">
-            <p>GitHub</p>
+        <a href="https://github.com/Jwoll2004/resume-builder" target="_blank" rel="nopener noreferrer">
+          <img src={github} alt="github" className="icons monoicons"/>
+          Github
         </a>
     </div>
   );
